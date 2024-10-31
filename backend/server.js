a3208e1294f1fd4c6285ae69f94c040f871939bc
Shad Sheikh <shadsheikh3107@gmail.com>
2024-10-31 15:56:44 +0530

    update dev command

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,30 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import { connectDB } from './config/db.js'
-import productRoutes from "./routes/product.route.js"
-import path from 'path'
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
 
-const app = express()
-const PORT = process.env.PORT || 3000
+import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(express.json()); //alows us to accept JSON data in the req.body
-app.use("/api/products", productRoutes)
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.use("/api/products", productRoutes);
+
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-    })
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
 }
 
 app.listen(PORT, () => {
     connectDB();
-    console.log("server started at http://localhost:" + PORT)
-})
+    console.log("Server started at http://localhost:" + PORT);
+});
